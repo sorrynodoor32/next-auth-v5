df3bcd434d40b8854c65b4b8b858e3d73e3bb46b
sorrynodoor32 <89461242+sorrynodoor32@users.noreply.github.com>
2024-04-29 14:58:25 +0700

    #4-auth-callbacks

--- auth.mjs
+++ auth.mjs
@@ -2,9 +2,35 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
 
-export const {auth, handlers, signIn, signOut} = NextAuth({
-    adapter: PrismaAdapter(db),
-    session: {strategy: 'jwt'},
-    ...authConfig,
+export const {auth, handlers, signIn, signOut} = 
+    NextAuth({
+        callbacks: {
+            async session({token, session}){
+               
+                if(token.sub && session.user){
+                    session.user.id = token.sub
+                }
+
+                if(token.role && session.user){
+                    session.user.role = token.role
+                }
+                return session
+            },
+            async jwt({token}){
+                if(!token.sub) return token
+
+                const existingUser = await getUserById(token.sub)
+
+                if(!existingUser) return token
+
+                token.role = existingUser.role
+
+                return token
+            }
+        },
+        adapter: PrismaAdapter(db),
+        session: {strategy: 'jwt'},
+        ...authConfig,
 })
